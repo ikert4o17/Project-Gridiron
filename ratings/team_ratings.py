86e5c71e741aa5a13293d4d3bda2709c75ec7daa
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-27 16:06:33 -0500

    Update team_ratings.py

--- ratings/team_ratings.py
+++ ratings/team_ratings.py
@@ -2,27 +2,32 @@
 Project Gridiron
 Team Rating Engine
 
-Version 0.1
+Version 0.2
 """
 
-teams = {
-    "Example Team": {
-        "offense": 85,
-        "defense": 80,
-        "strength_of_schedule": 75
-    }
-}
+from data.teams import nfl_teams, college_teams
 
 
 def calculate_rating(team):
     rating = (
         team["offense"] * 0.4 +
-        team["defense"] * 0.4 +
-        team["strength_of_schedule"] * 0.2
+        team["defense"] * 0.4
     )
 
     return round(rating, 2)
 
 
-for name, stats in teams.items():
-    print(name, calculate_rating(stats))
+print("NFL Rankings")
+print("----------------")
+
+for team in nfl_teams:
+    rating = calculate_rating(team)
+    print(team["name"], rating)
+
+
+print("\nCollege Football Rankings")
+print("----------------")
+
+for team in college_teams:
+    rating = calculate_rating(team)
+    print(team["name"], rating)
