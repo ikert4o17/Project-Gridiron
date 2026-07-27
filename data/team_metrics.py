78221b49878ab502a399f62ca1669526c5f8d6ea
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-27 16:16:10 -0500

    Create team_metrics.py

---
+++ data/team_metrics.py
@@ -0,0 +1,41 @@
+"""
+Project Gridiron
+Team Metrics Database
+
+Version 0.1
+"""
+
+nfl_metrics = [
+    {
+        "name": "Kansas City Chiefs",
+        "points_per_game": 28.5,
+        "points_allowed": 21.0,
+        "yards_per_play": 6.1,
+        "turnover_margin": 5
+    },
+    {
+        "name": "Philadelphia Eagles",
+        "points_per_game": 27.2,
+        "points_allowed": 19.8,
+        "yards_per_play": 5.9,
+        "turnover_margin": 8
+    }
+]
+
+
+college_metrics = [
+    {
+        "name": "Georgia Bulldogs",
+        "points_per_game": 34.5,
+        "points_allowed": 16.2,
+        "yards_per_play": 6.4,
+        "turnover_margin": 10
+    },
+    {
+        "name": "Ohio State Buckeyes",
+        "points_per_game": 35.1,
+        "points_allowed": 17.5,
+        "yards_per_play": 6.3,
+        "turnover_margin": 7
+    }
+]
