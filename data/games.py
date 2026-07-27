5f8ab961e512c3e843e6e9f22634076aedd8b95a
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-27 16:12:04 -0500

    Create games.py

---
+++ data/games.py
@@ -0,0 +1,33 @@
+"""
+Project Gridiron
+Weekly Game Schedule
+
+Version 0.1
+"""
+
+nfl_week = [
+    {
+        "home": "Kansas City Chiefs",
+        "away": "Buffalo Bills",
+        "week": 1
+    },
+    {
+        "home": "Philadelphia Eagles",
+        "away": "Dallas Cowboys",
+        "week": 1
+    }
+]
+
+
+college_week = [
+    {
+        "home": "Georgia Bulldogs",
+        "away": "Alabama Crimson Tide",
+        "week": 1
+    },
+    {
+        "home": "Ohio State Buckeyes",
+        "away": "Michigan Wolverines",
+        "week": 1
+    }
+]
