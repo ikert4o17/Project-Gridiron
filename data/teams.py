c787f5131f37e1550095afe0df454d5c060bb1f2
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-27 16:04:38 -0500

    Create teams.py

---
+++ data/teams.py
@@ -0,0 +1,37 @@
+"""
+Project Gridiron
+Team Database
+
+Version 0.1
+"""
+
+nfl_teams = [
+    {
+        "name": "Kansas City Chiefs",
+        "conference": "AFC",
+        "offense": 90,
+        "defense": 85
+    },
+    {
+        "name": "Philadelphia Eagles",
+        "conference": "NFC",
+        "offense": 88,
+        "defense": 87
+    }
+]
+
+
+college_teams = [
+    {
+        "name": "Georgia Bulldogs",
+        "conference": "SEC",
+        "offense": 92,
+        "defense": 90
+    },
+    {
+        "name": "Ohio State Buckeyes",
+        "conference": "Big Ten",
+        "offense": 91,
+        "defense": 88
+    }
+]
