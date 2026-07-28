ba96a9a618622a2ea893176307698ac2c2950924
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-28 16:26:14 -0500

    Create teams.js

---
+++ website/teams.js
@@ -0,0 +1,17 @@
+const teams = [
+    {
+        name: "Kansas City Chiefs",
+        rating: 1500,
+        rank: 1
+    },
+    {
+        name: "Philadelphia Eagles",
+        rating: 1500,
+        rank: 2
+    },
+    {
+        name: "Buffalo Bills",
+        rating: 1500,
+        rank: 3
+    }
+];
