2ce84c9cfa6c3d3cc64e353f0fff697d9e9cfda5
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-28 16:21:05 -0500

    Create data.js

---
+++ website/data.js
@@ -0,0 +1,23 @@
+const rankings = [
+    {
+        team: "Kansas City Chiefs",
+        rating: 1500
+    },
+    {
+        team: "Philadelphia Eagles",
+        rating: 1500
+    },
+    {
+        team: "Buffalo Bills",
+        rating: 1500
+    }
+];
+
+
+const predictions = [
+    {
+        matchup: "Chiefs vs Bills",
+        pick: "Kansas City Chiefs",
+        probability: "62%"
+    }
+];
