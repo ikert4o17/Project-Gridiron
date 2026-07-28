889381f47c106fe940efb615390dc1323bff20e3
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-28 11:09:41 -0500

    Create results.py

---
+++ data/results.py
@@ -0,0 +1,21 @@
+"""
+Project Gridiron
+Game Results Database
+
+Version 1.0
+"""
+
+nfl_results = [
+    {
+        "winner": "Kansas City Chiefs",
+        "loser": "Buffalo Bills",
+        "winner_score": 27,
+        "loser_score": 24
+    },
+    {
+        "winner": "Philadelphia Eagles",
+        "loser": "Dallas Cowboys",
+        "winner_score": 31,
+        "loser_score": 20
+    }
+]
