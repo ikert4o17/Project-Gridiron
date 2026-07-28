f881df0849912b442117a004408aceea316d87cb
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-28 11:07:29 -0500

    Create nfl_ratings.py

---
+++ data/nfl_ratings.py
@@ -0,0 +1,43 @@
+"""
+Project Gridiron
+NFL Elo Ratings
+
+Version 1.0
+"""
+
+from ratings.elo import DEFAULT_ELO
+
+nfl_ratings = {
+    "Arizona Cardinals": DEFAULT_ELO,
+    "Atlanta Falcons": DEFAULT_ELO,
+    "Baltimore Ravens": DEFAULT_ELO,
+    "Buffalo Bills": DEFAULT_ELO,
+    "Carolina Panthers": DEFAULT_ELO,
+    "Chicago Bears": DEFAULT_ELO,
+    "Cincinnati Bengals": DEFAULT_ELO,
+    "Cleveland Browns": DEFAULT_ELO,
+    "Dallas Cowboys": DEFAULT_ELO,
+    "Denver Broncos": DEFAULT_ELO,
+    "Detroit Lions": DEFAULT_ELO,
+    "Green Bay Packers": DEFAULT_ELO,
+    "Houston Texans": DEFAULT_ELO,
+    "Indianapolis Colts": DEFAULT_ELO,
+    "Jacksonville Jaguars": DEFAULT_ELO,
+    "Kansas City Chiefs": DEFAULT_ELO,
+    "Las Vegas Raiders": DEFAULT_ELO,
+    "Los Angeles Chargers": DEFAULT_ELO,
+    "Los Angeles Rams": DEFAULT_ELO,
+    "Miami Dolphins": DEFAULT_ELO,
+    "Minnesota Vikings": DEFAULT_ELO,
+    "New England Patriots": DEFAULT_ELO,
+    "New Orleans Saints": DEFAULT_ELO,
+    "New York Giants": DEFAULT_ELO,
+    "New York Jets": DEFAULT_ELO,
+    "Philadelphia Eagles": DEFAULT_ELO,
+    "Pittsburgh Steelers": DEFAULT_ELO,
+    "San Francisco 49ers": DEFAULT_ELO,
+    "Seattle Seahawks": DEFAULT_ELO,
+    "Tampa Bay Buccaneers": DEFAULT_ELO,
+    "Tennessee Titans": DEFAULT_ELO,
+    "Washington Commanders": DEFAULT_ELO
+}
