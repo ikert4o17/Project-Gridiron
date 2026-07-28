084ded72fedfc9f842bbd28ffbea56268116fd5d
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-28 16:10:03 -0500

    Update matchup_predictor.py

--- predictions/matchup_predictor.py
+++ predictions/matchup_predictor.py
@@ -1,52 +1,5 @@
-"""
-Project Gridiron
-Matchup Predictor
+def elo_win_probability(home_rating, away_rating):
 
-Version 2.0
-"""
-
-from models.rating_storage import load_ratings
-
-
-HOME_FIELD = 50
-
-
-def predict_matchup(home_team, away_team):
-
-    ratings = load_ratings()
-
-    home_rating = ratings[home_team] + HOME_FIELD
-    away_rating = ratings[away_team]
-
-    difference = home_rating - away_rating
-
-    win_probability = 50 + (difference / 10)
-
-    if win_probability > 95:
-        win_probability = 95
-
-    if win_probability < 5:
-        win_probability = 5
-
-    winner = (
-        home_team
-        if difference > 0
-        else away_team
+    return 1 / (
+        1 + 10 ** ((away_rating - home_rating) / 400)
     )
-
-    return {
-        "winner": winner,
-        "win_probability": round(win_probability, 1),
-        "rating_difference": round(difference, 1)
-    }
-
-
-if __name__ == "__main__":
-
-    prediction = predict_matchup(
-        "Kansas City Chiefs",
-        "Buffalo Bills"
-    )
-
-    print("🏈 PROJECT GRIDIRON MATCHUP")
-    print(prediction)
