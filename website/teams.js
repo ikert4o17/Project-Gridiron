8936a254fd02ba2c517460da3389aa77c0d6ca56
ikert4o17 <bhrzrsyssn@privaterelay.appleid.com>
2026-07-29 21:08:47 -0500

    Update teams.js

--- website/teams.js
+++ website/teams.js
@@ -2,16 +2,19 @@ const teams = [
     {
         name: "Kansas City Chiefs",
         conference: "AFC",
-        rating: 1500
+        rating: 1532,
+        trend: "Rising"
     },
     {
         name: "Philadelphia Eagles",
         conference: "NFC",
-        rating: 1500
+        rating: 1518,
+        trend: "Steady"
     },
     {
         name: "Buffalo Bills",
         conference: "AFC",
-        rating: 1500
+        rating: 1507,
+        trend: "Falling"
     }
 ];
